654cc5c366f0fb0241ea6c59e35b9c1785b43a52
David Cain <davidjosephcain@gmail.com>
2020-11-22 14:43:35 -0800

    Add integrity hashes, note CSP rules
    Even though we're serving up scripts from our own domain and *not* a
    CDN, it's very little effort to just add a hash as part of the
    `<script>` tag for a bit of free security.
    
    Additionally, this project is deployed with a CSP. Because this
    repository only has JavaScript source (and doesn't deal with giving
    headers), it's worth noting our expectation of compatibility.
    
    For reference, here's the full CSP I serve with this project
    (newlines added for readability):
    
        default-src 'none';
        base-uri 'self';
        connect-src 'self' https://api.dcain.me;
        form-action 'self';
        frame-ancestors 'none';
        img-src 'self' data: https://secure.syndetics.com;
        script-src 'self';
        style-src 'self'
    
    This can be easily tested locally with `serve`:
    
    1. First, configure serve to give the headers:
    
        ```json
        # public/serve.json
        {
          "headers": [
            {
              "source": "**/*",
              "headers": [
                {
                  "key": "Content-Security-Policy",
                  "value": "base-uri 'self'; ... style-src 'self'"
                }
              ]
            }
          ]
        }
        ```
    
    2. Build bundle:
    
        ```bash
         npm run build
        ```
    
    3. Serve, check console for errors:
    
        ```bash
        npx serve dist
        ```

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  integrity: true,
+  // The full build of Vue needs unsafe-eval in the CSP so that it may compile templates from strings.
+  // We don't need that behavior, since all templates are `.vue` files.
+  // The default setting of this is already false, but we're explicit about our dependence on it.
+  runtimeCompiler: false
 };
