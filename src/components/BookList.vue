2325f34721c7f5992e1646184f6c3668a808714e
David Cain <davidjosephcain@gmail.com>
2020-11-22 16:29:04 -0800

    Add a small notice explaining few matches
    I'm really hopeful that the Goodreads folks will grant me access to the
    `work.versions` endpoint, since it would really help improve the search
    algorithm... we shall see.
    
    In the meantime, add a small notice explaining why results aren't that
    great.

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -13,6 +13,19 @@
     <div class="alert alert-warning" v-if="noResultsFound">
       Nothing found on the shelf!
     </div>
+    <div class="alert alert-info" v-if="fewResultsFound">
+      <strong>Expecting more matches?</strong>
+      <p>
+        The search algorithm currently searches for an <em>exact</em>
+        match on the ISBNs from your Goodreads shelf. A better algorithm is in
+        the works, where we favor exact ISBN matches but fall back to other
+        versions.
+      </p>
+      <p>
+        In the meantime, you might try swapping the ISBN "version" of any books
+        on your list (e.g. from paperback to hardcover).
+      </p>
+    </div>
   </div>
 </template>
 
@@ -39,6 +52,9 @@ export default Vue.extend({
   computed: {
     noResultsFound(): boolean {
       return this.state === QueryState.COMPLETED && !this.books.length;
+    },
+    fewResultsFound(): boolean {
+      return this.state === QueryState.COMPLETED && this.books.length < 5;
     }
   },
   data() {
